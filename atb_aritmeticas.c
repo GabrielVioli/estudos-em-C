d9835d5492c835afa18258c01cc88f12700768a0
GabrielVioli <gabrielgvo16@gmail.com>
2023-11-16 22:54:31 -0300

    tentativa de codigo pra divisão, erro de contagem

--- atb_aritmeticas.c
+++ atb_aritmeticas.c
@@ -4,10 +4,20 @@
 int main() {
 
     int init_cont = 0;
+    int init_cont2 = 0;
+    int user_input;
+    int user_input2;
 
-    while (init_cont != 100) {
-        printf("%d", init_cont);
-        init_cont = init_cont + 1;
+    printf("divisor: ");
+    scanf("%d", &user_input2);
+    printf("dividendo: ");
+    scanf("%d", &user_input);
+
+
+    while (init_cont != user_input) {
+        printf("%d\n%d\n", init_cont, init_cont2);
+        init_cont += user_input2;
+        init_cont2++;
     }
     
     return 0;
