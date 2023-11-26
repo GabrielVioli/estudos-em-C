78b2b5d40f4498c49b580070f7a9bb3ed0a1fa36
GabrielVioli <gabrielgvo16@gmail.com>
2023-11-25 22:25:46 -0300

    adicionado arquivo novo

---
+++ senha.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char temp_code[20];
+    char code[20];
+
+    printf("senha: ");
+    scanf("%s", temp_code);
+
+    printf("repita a senha: ");
+    scanf("%s", code);
+
+    if(strcmp(temp_code, code) != 0) {
+        printf("senhas nao conhecidem\n");
+
+        while (strcmp(temp_code, code) != 0)
+        {
+            printf("senha: ");
+            scanf("%s", temp_code);
+
+            printf("repita a senha: ");
+            scanf("%s", code);
+
+            if(strcmp(temp_code, code) != 0) {
+                printf("senhas nao conhecidem\n");
+            }
+
+            if(strcmp(temp_code, code) == 0) {
+                printf("Sucesso!\n");
+                break;
+            }
+
+        }
+        
+    }
+
+    char senha[20];
+
+    printf("senha: ");
+    scanf("%s", senha);
+
+    if(strcmp(senha, code) != 0) {
+        printf("senha incorreta");
+
+        while (strcmp(senha, code) != 0)
+        {
+            printf("senha: ");
+            scanf("%s", senha);
+            
+            if(strcmp(senha, code) == 0) {
+                printf("Sucesso!");
+                break;
+            }
+        }
+        
+    
+    }
+
+    else
+    {
+        printf("Sucesso!");
+    }
+    
+
+}
